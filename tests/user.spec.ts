68b8e911c7278e06aaab870e9f186200a5ada183
austinbeckstead <austinbeckstead314@gmail.com>
2026-02-16 17:13:54 -0700

    added list/delete user tests

--- tests/user.spec.ts
+++ tests/user.spec.ts
@@ -51,4 +51,48 @@ test('updateUser', async ({ page }) => {
 
 test ('list users in admin dashboard', async ({ page }) => {
 
+    // Login as admin
+    const email = `user${Math.floor(Math.random() * 10000)}@jwt.com`;
+    await page.goto('/');
+    await register(page, 'pizza diner', email, 'diner', true);
+    await login(page, email, 'diner');
+    await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible();
+
+    // Open admin dashboard
+    await page.getByRole('link', { name: 'Admin' }).click();
+    await expect(page.getByRole('list')).toContainText('admin-dashboard');
+
+    // Check that the user we just created is in the user list
+    await expect(page.getByRole('table')).toContainText(email);
+
+});
+
+test('delete user', async ({ page }) => {
+
+    // Register a new user
+    const email = `user${Math.floor(Math.random() * 10000)}@jwt.com`;
+    const adminEmail = `admin${Math.floor(Math.random() * 10000)}@jwt.com`;
+    await page.goto('/');
+    await register(page, 'pizza diner', email, 'diner');
+    await page.getByRole('link', { name: 'Logout' }).click();
+    await register(page, 'admin', adminEmail, 'diner', true);
+    await login(page, adminEmail, 'diner');
+    await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible();
+
+    // Open admin dashboard
+    await page.getByRole('link', { name: 'Admin' }).click();
+    await expect(page.getByRole('list')).toContainText('admin-dashboard');
+
+    // Delete the user we just created
+    const userRow = page.getByRole('row', { name: new RegExp(email) });
+    const deleteButton = userRow.getByRole('button', { name: 'Delete' });
+    await expect(deleteButton).toBeVisible();
+    await deleteButton.click();
+
+    // Confirm deletion
+    await expect(page.locator('h3')).toContainText('Delete user');
+    await page.getByRole('button', { name: 'Delete' }).click();
+
+    // Check that the user is no longer in the user list
+    await expect(page.getByRole('table')).not.toContainText(email);
 });
